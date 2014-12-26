fef4df82beeec88e13c6c412b4cca6966c6f6087
Canh Tran <canh.tran@go1.com.au>
2014-12-26 10:51:57 +0700

    Edit header file

--- parts/header.php
+++ parts/header.php
@@ -8,7 +8,6 @@
 <link rel="author" href="https://plus.google.com/+C%E1%BA%A3nhTr%E1%BA%A7nDuy">
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
-<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/images/favicon.png" />
 <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet" />
@@ -21,13 +20,14 @@
         <div class="col-md-12 col-md-offset-4">
 	        <h1>
 	        	<a title="<?php bloginfo('name'); ?>" href="<?php echo get_option('home'); ?>">
-	            <img src="<?php bloginfo('template_directory'); ?>/images/logo.png" alt="<?php bloginfo('name'); ?>"/>
+	            <img class="img-responsive" src="<?php bloginfo('template_directory'); ?>/images/logo.png" alt="<?php bloginfo('name'); ?>" title="<?php bloginfo('name'); ?>"/>
 	          </a>
 	         </h1>
         </div>
       </div>
     </div>
   </div>
+
 	<div class="navbar navbar-default">
 	  <div class="navbar-header">
 	    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
@@ -36,13 +36,12 @@
 	      <span class="icon-bar"></span>
 	    </button>
 	  </div>
-	  <div class="navbar-collapse collapse navbar-responsive-collapse'">
-	  	<?php wp_nav_menu( array( 
-    	'theme_location' => 'header-menu', 
-    	'container' => 'none',
-    	'menu_class'	=> 'nav navbar-nav' 
-    ) ); ?>	
-	  </div>
-	</div>
- 
+    <div class="navbar-collapse collapse navbar-responsive-collapse">
+      <?php wp_nav_menu( array(
+        'theme_location' => 'header-menu',
+        'container' => 'none',
+        'menu_class' => 'nav navbar-nav'
+      ) ); ?>
+    </div>
+  </div>
 
