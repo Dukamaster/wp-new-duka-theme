a93be3cb382dfdfbbfedb970bb3c0279123a1434
Canh Tran <trandcanh@gmail.com>
2015-04-09 10:29:25 +0700

    Change widget layout

--- functions/widgets.php
+++ functions/widgets.php
@@ -5,7 +5,7 @@ function dukaweb_widgets_init() {
     'name' => 'Right sidebar',
     'id' => 'right-sidebar',
     'description' => 'Appears in the sidebar',
-    'before_widget' => '<div id="%1$s" class="widget %2$s panel panel-info">',
+    'before_widget' => '<div id="%1$s" class="widget %2$s">',
     'after_widget' => '</div>',
     'before_title' => '',
     'after_title' => '',
@@ -13,18 +13,18 @@ function dukaweb_widgets_init() {
 }
 add_action('widgets_init', 'dukaweb_widgets_init');
 
-//Add class to widget sidebars.
-add_filter('widget_title', 'widget_title');
-function widget_title( $title ) {
-  if ( $title )
-      $title = '<div class="panel-heading"><h3 class="panel-title">' . $title . '</h3></div>';
-  $title .= '<div class="panel-body">';
-  return $title;
-}
+// //Add class to widget sidebars.
+// add_filter('widget_title', 'widget_title');
+// function widget_title( $title ) {
+//   if ( $title )
+//       $title = '<div class="panel-heading"><h3 class="panel-title">' . $title . '</h3></div>';
+//   $title .= '<div class="panel-body">';
+//   return $title;
+// }
 
-//Wrap the close div class to widget sidebar
-add_filter('dynamic_sidebar_params', 'dynamic_sidebar_params');
-function dynamic_sidebar_params( $params ) {
-  $params[0]['after_widget'] = '</div>' . $params[0]['after_widget'];
-  return $params;
-}
+// //Wrap the close div class to widget sidebar
+// add_filter('dynamic_sidebar_params', 'dynamic_sidebar_params');
+// function dynamic_sidebar_params( $params ) {
+//   $params[0]['after_widget'] = '</div>' . $params[0]['after_widget'];
+//   return $params;
+// }
