52ad4b8c628e372d4a76d60806b60e7d1b3a46e7
Canh Tran <trandcanh@gmail.com>
2015-04-09 23:52:53 +0700

    Add code sidebar

--- sidebar.php
+++ sidebar.php
@@ -2,7 +2,7 @@
   <?php if(is_active_sidebar('right-sidebar')){
     dynamic_sidebar('right-sidebar');}
   ?>
-  <?php if(is_active_sidebar('sidebar-ads')){
+  <?php if(is_active_sidebar('ads-sidebar')){
     dynamic_sidebar('ads-sidebar');}
   ?>
 </div>
